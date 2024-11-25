0ebb2f6dd93d97b560f20df330763b8a48a36d55
niux3 <renaudbourdeau@gmail.com>
2024-11-25 20:59:44 +0100

    conference: chapt-2-3

---
+++ front/chapt-2-3/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Select from './lib/Select.svelte'
+    let selected = $state('')
+    let options = $state([
+        {label: 'Vert'},
+        {label: 'Jaune'},
+        {label: 'Rouge'},
+        {label: 'Orange'},
+        {label: 'Violet'},
+        {label: 'Rose'},
+    ])
+    let onChange = e => selected = e.target.value
+</script>
+
+<Select options={options} onChange={onChange} />
+<p>{selected}</p>
---
+++ front/chapt-2-3/src/lib/Select.svelte
@@ -0,0 +1,13 @@
+<script>
+    let {options, onChange} = $props()
+</script>
+
+<div>
+    <label for="color">Couleurs</label>
+    <select id="colors" class="form-control" onchange={onChange}>
+        <option value="">Choisir une couleur</option>
+        {#each options as option}
+            <option value={option.label}>{option.label}</option>
+        {/each}
+    </select>
+</div>
