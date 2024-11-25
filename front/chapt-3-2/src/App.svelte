620eb133410c95d742aaf87278f3bd5d294c6324
niux3 <renaudbourdeau@gmail.com>
2024-11-25 22:09:47 +0100

    conferenced: chapitre 3

---
+++ front/chapt-3-2/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Navigation from './lib/Navigation.svelte'
+
+    let data = $state(async ()=>{
+        let response = await fetch('/data.json');
+        if(response.ok){
+            let data = await response.json();
+            return data;
+        } else {
+            throw new Error("le serveur ne répond pas");
+        }
+    })
+</script>
+
+<Navigation {data} />
---
+++ front/chapt-3-3/src/lib/Navigation.svelte
@@ -0,0 +1,23 @@
+<script>
+    let { data } = $props()
+</script>
+<nav>
+    {#await data}
+    <p>chargement....</p>
+    {:then rows}
+    <ul>
+        {#each rows as row}
+        <li>
+            <strong>{row.name}</strong>
+            <ul>
+                {#each row.subcategory as category}
+                <li><a href="/{row.slug}/{category.slug}">{category.name}</a></li>
+                {/each}
+            </ul>
+        </li>
+    {/each}
+    </ul>
+    {:catch error}
+        <div class="error">Error: {error.message}</div>
+    {/await}
+</nav>
