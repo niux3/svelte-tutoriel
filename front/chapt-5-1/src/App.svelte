4bc7a777e2ce33bff6b9708a310b9bed13ea6067
niux3 <renaudbourdeau@gmail.com>
2024-11-27 11:53:25 +0100

    conference : chapite 5

--- front/chapt-5-1/src/App.svelte
+++ front/chapt-5-1/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
     import Counter from './lib/Counter.svelte'
+    import CounterAutoSubscribe from './lib/CounterAutoSubscribe.svelte'
     import CounterSubscribe from './lib/CounterSubscribe.svelte'
     import Reset from './lib/Reset.svelte'
 </script>
@@ -7,6 +8,8 @@
 <Counter></Counter>
 <Counter></Counter>
 <Counter></Counter>
+<hr>
+<CounterAutoSubscribe></CounterAutoSubscribe>
 <hr>
 <Reset></Reset>
 <hr>
---
+++ front/chapt-5-1/src/lib/CounterAutoSubscribe.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { count } from '../stores.js'
+</script>
+
+<button class="btn btn-primary" onclick={e => $count++}>
+    <span>Click : </span>
+    <span>{$count}</span>
+</button>
