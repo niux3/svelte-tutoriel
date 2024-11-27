1b4ec8ea34c7b77cb4ca563e7374ea1e41f60834
niux3 <renaudbourdeau@gmail.com>
2024-11-27 16:44:02 +0100

    conference: chapitre 7

---
+++ front/chapt-7-1/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Demo from './lib/Demo.svelte';
+
+    let show = $state(false);
+</script>
+
+<label>
+    <input type="checkbox" bind:checked={show}>
+    Show
+</label>
+{#if show}
+    <Demo />
+{/if}
